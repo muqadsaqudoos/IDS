87480207273e8f1706b3907dbce5bd4cf1c29074
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2024-10-13 01:05:11 +0500

    part2

--- Assignemnt2/assignment2.py
+++ Assignemnt2/assignment2.py
@@ -3,10 +3,11 @@ def user_names(list_1):
     new_list = []
     for user in list_1:
         if (user[2]>30) and (user[3] == "USA" or user[3] == "Canada"):
+            list_1.pop(user)
             new_list.append(user[1])
 
 
-    return new_list
+    return new_list,list_1
 
 def age_sort(list_1):
     duplicates = []
@@ -19,5 +20,47 @@ def age_sort(list_1):
             print(i[1])
 
     return sorted_age[-1:-11:-1]
+
+#part2
+
+def total_number(list_2):
+    count = 1
+    users = []
+    for transaction in list_2:
+        if transaction[1] not in users:
+            count += 1
+        users.append(transaction[1])
+
+    return count
+
+def highest_amount(list_2):
+    max = 0 
+    for transaction in list_2:
+        if max < transaction[2]:
+            max = transaction[2]
+
+    return max
+
+def two_tuples(list_2):
+    users_list = []
+    transactions_list = []
+    for transaction in list_2:
+        for j in range(len(transaction)):
+            if j == 1:
+                users_list.append(transaction[j])
+            if j == 0:
+                transactions_list.append(transaction[j])
+
+    return users_list,transactions_list
+
+#part3
+
+
+
+
+
+
+
+
 
 
